81c74593869b81e0066297b0b7d9960ff6b10c4f
Rudy Matos <rudymatos.dev@gmail.com>
2019-06-04 09:14:54 -0400

    Adding test for QueryParam

--- ios-code-challengeTests/CCTest.swift
+++ ios-code-challengeTests/CCTest.swift
@@ -7,11 +7,45 @@
 //
 
 import XCTest
+@testable import ios_code_challenge
 
 class CCTest: XCTestCase {
 
+    func testQueryParamTypeShouldBeAddressWithInit(){
+        let queryParam = QueryParam()
+        XCTAssertEqual(queryParam.type, QueryParam.QueryType.address)
+    }
 
-    func testExample() {
+    func testQueryParamShouldContainDefaultAddressWhenInit(){
+        let queryParam = QueryParam()
+        XCTAssertEqual(queryParam.location, "5550 West Executive Dr. Tampa, FL 33609")
+    }
+    
+    func testQueryParamShouldChangeTypeWhenAssigningLatitudeAndLogitude(){
+        var queryParam = QueryParam()
+        XCTAssertEqual(queryParam.type, QueryParam.QueryType.address)
+        queryParam.set(latitude: 0, longitude: 0)
+        XCTAssertEqual(queryParam.type, QueryParam.QueryType.location)
+    }
+
+    
+    func testQueryParamShouldContainLocationValueWhenIsAddressType(){
+        let queryParam = QueryParam()
+        XCTAssertNotNil(queryParam.getQueryDict()["location"])
+    }
+    
+    
+    func testQueryParamShouldContainNotLocationValueWhenIsNotAddressType(){
+        var queryParam = QueryParam()
+        queryParam.set(latitude: 0, longitude: 0)
+        XCTAssertNil(queryParam.getQueryDict()["location"])
+    }
+    
+    func testQueryParamShouldContainLatAndLonWhenIsLocationType(){
+        var queryParam = QueryParam()
+        queryParam.set(latitude: 0, longitude: 0)
+        XCTAssertNotNil(queryParam.getQueryDict()["latitude"])
+        XCTAssertNotNil(queryParam.getQueryDict()["longitude"])
     }
 
 }
